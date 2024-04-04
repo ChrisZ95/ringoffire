bc36c45db9861b5ce9bfd775f820d25c6cda85db
ChrisZ95 <160311410+ChrisZ95@users.noreply.github.com>
2024-04-04 14:36:06 +0200

    Finished

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,8 +12,9 @@ import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player
 import { MatDialogModule } from '@angular/material/dialog';
 import { GameInfoComponent } from '../game-info/game-info.component';
 import { inject } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, addDoc, doc, getDoc, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
@@ -23,42 +24,59 @@ import { Observable } from 'rxjs';
   styleUrl: './game.component.scss'
 })
 export class GameComponent implements OnInit {
-  pickCardAnimation = false;
-  currentCard: string = '';
   game: Game = new Game();
   firestore: Firestore = inject(Firestore);
   items$: Observable<any[]>;
   aCollection = collection(this.firestore, 'games')
 
-  constructor(public dialog: MatDialog){
+  constructor(private route: ActivatedRoute, public dialog: MatDialog){
     const aCollection = collection(this.firestore, 'games')
     this.items$ = collectionData(aCollection);
   }
 
   ngOnInit(): void {
     this.newGame();
-    collectionData(this.aCollection).subscribe((game) => {
-      console.log('Game update', game);
-    });
+    this.route.params.subscribe((params) => {
+      console.log(params);
+
+      const docRef = doc(this.aCollection, params['id']);
+      getDoc(docRef).then((snapshot) => {
+        console.log('Game update', snapshot.data());
+        const gameData = snapshot.data();
+        if (gameData) {
+          this.game.currentPlayer = gameData['currentPlayer']; 
+          this.game.playedCards = gameData['playedCards'];
+          this.game.players = gameData['players'];
+          this.game.stack = gameData['stack'];
+          this.game.pickCardAnimation = gameData['pickCardAnimation'];
+          this.game.currentCard = gameData['currentCard'];
+        } else {
+          console.error("No data found for game.");
+        }
+      }).catch(error => {
+        console.error("Error getting document:", error);
+      });
+    })
   }
 
   newGame(){
     this.game = new Game();
-    console.log(this.game);
   }
 
   takeCard(){
     const poppedCard = this.game.stack.pop();
-    if (!this.pickCardAnimation && poppedCard !== undefined) {
-      this.currentCard = poppedCard;
-      this.pickCardAnimation = true;
-      console.log('New Card: ', this.currentCard);
-      console.log('Game is: ', this.game)
+    if (!this.game.pickCardAnimation && poppedCard !== undefined) {
+      this.game.currentCard = poppedCard;
+      this.game.pickCardAnimation = true;
+      console.log('New Card: ', this.game.currentCard);
+      console.log('Game is: ', this.game);
       this.game.currentPlayer++;
       this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      this.saveGame();
       setTimeout(()=>{
-        this.game.playedCards.push(this.currentCard);
-        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.game.currentCard);
+        this.game.pickCardAnimation = false;
+        this.saveGame();
       }, 1000)
     } else {
       console.error("Keine weiteren Karten im Stapel!");
@@ -71,7 +89,22 @@ export class GameComponent implements OnInit {
     dialogRef.afterClosed().subscribe((name: string) => {
       if (name && name.length > 0) {
         this.game.players.push(name);
+        this.saveGame();
       }
     });
+  }
+
+  saveGame() {
+    const gameId = this.route.snapshot.params['id'];
+    const gameDocRef = doc(this.aCollection, gameId);
+    const gameData = this.game.toJson();
+
+    updateDoc(gameDocRef, gameData)
+      .then(() => {
+        console.log("Spiel erfolgreich gespeichert!");
+      })
+      .catch((error) => {
+        console.error("Fehler beim Speichern des Spiels:", error);
+      });
   }
 }
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Firestore, collection, collectionData, addDoc, doc, getDoc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-start-screen',
@@ -9,14 +13,21 @@ import { Router } from '@angular/router';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
-
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+  aCollection = collection(this.firestore, 'games')
+  
   constructor(private router: Router){
-
+    const aCollection = collection(this.firestore, 'games')
+    this.items$ = collectionData(aCollection);
   }
 
   newGame(){
     // Start game
-    this.router.navigateByUrl('/game');
+    let game = new Game();
+    addDoc(this.aCollection, game.toJson()).then((gameInfo: any) => {
+      this.router.navigateByUrl('/game/' + gameInfo['id']);
+    });
   }
 
 }
